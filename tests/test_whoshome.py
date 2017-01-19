467b50333ab4dd16f5058ed67a171eef8c86bd4f
RhysDeimel <deimelr@hotmail.com>
2017-01-19 14:05:27 +1100

    testing setup

---
+++ tests/test_whoshome.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestWhosHome(unittest.TestCase):
+
+	def test_upper(self):
+		self.assertEqual('foo'.upper(), 'FOO')
+
+	def test_smoke_test(self):
+		self.assertEqual(2, 3)
+
+
+if __name__ == '__main__':
+	unittest.main()
--- whoshome.py
+++ whoshome.py
@@ -1,9 +1,14 @@
 import subprocess
 
 sausage = subprocess.Popen(["arp-scan", "-l"], stdout=subprocess.PIPE,
-	universal_newlines=True)
+	universal_newlines=False
+	)
 
 test = sausage.stdout
 
 for line in test:
 	print(line)
+
+print("="*10)
+
+print(test.readline())
