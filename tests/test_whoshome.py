36f61d6050065a61c5b1ece42b742a9938fd6c94
RhysDeimel <deimelr@hotmail.com>
2017-01-19 16:47:00 +1100

    Added extra test for Jenkins webhook integration

--- tests/test_whoshome.py
+++ tests/test_whoshome.py
@@ -8,6 +8,9 @@ class TestWhosHome(unittest.TestCase):
 	def test_smoke_test(self):
 		self.assertEqual(2, 2)
 
+	def test_testy_test_pants(self):
+		self.assertNotEqual(2, 3)
+
 
 if __name__ == '__main__':
 	unittest.main()
