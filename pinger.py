23534781cbaae18e675bc414f74cf7d03a1bf33b
RhysDeimel <deimelr@hotmail.com>
2017-02-26 16:53:46 +1100

    new method working. Time to test on raspi

--- pinger.py
+++ pinger.py
@@ -2,19 +2,9 @@ import data
 import shelve
 import subprocess
 import datetime
+import copy
+from jinja2 import FileSystemLoader, Environment
 
-user_list = ['kristen', 'rhys']
-
-''' # trash this
-def get_device_ip_list():
-    ip_list = []
-    with shelve.open('userdb') as db:
-        for user in user_list: # loop through users
-            for device in db['users'][user]['device'].keys():
-                ip_list.append(db['users'][user]['device'][device]['ip'])
-
-    return ip_list
-'''
 
 def ping(host):
     """
@@ -30,9 +20,10 @@ def days_hours_minutes(td):
     return td.days, td.seconds//3600, (td.seconds//60)%60
 
 
-
 def runner():
-
+    """
+    Opens db and updates using ping() and days_hours_minutes()
+    """
     with shelve.open('userdb', writeback=True) as db:
         for user in db['users'].keys(): # loop users
             for ip_add in db['users'][user]['ip'].keys(): # loop ips
@@ -44,21 +35,37 @@ def runner():
                     date1 = db['users'][user]['ip'][ip_add]['seen'][0]
                     date2 = datetime.datetime.now()
                     td = date2 - date1
-                    print(td)
                     td = days_hours_minutes(td)
                     device = db['users'][user]['ip'][ip_add]['device']
                     db['users'][user]['ip'][ip_add]['seen'][1] = "{}'s {} last seen {} days, {} hours, and {} minutes ago".format(user.title(), device, td[0], td[1], td[2])
-                    print(db['users'][user]['ip'][ip_add]['seen'][1])
 
 
+def render_from_template(directory, template_name, **kwargs):
+    loader = FileSystemLoader(directory)
+    env = Environment(loader=loader)
+    template = env.get_template(template_name)
+    return template.render(**kwargs)
 
 
-'''
+with shelve.open('userdb') as db:
+    jinja_data = {
+        'names': ['kristen', 'marc', 'rhys', 'sahar'],
+        'userdb': copy.deepcopy(db['users']),
+        'updated': datetime.datetime.now()
+    }
 
-populate jinja
-move file
+with open('index.html', 'w') as outfile:
+    html = render_from_template(".", "index_template.html", **jinja_data)
+    outfile.write(html)
 
-'''
+
+try:
+    target = os.path.join('/usr','share','nginx','www','whoshome','index.html')
+    print('Moving {} to {}'.format('index.html', target))
+    shutil.copyfile('index.html', target)
+except Exception as e:
+    print('failed')
+    print(e)
 
 
 
