079e665291bd218d8974d821b843e74c3f6093d7
RhysDeimel <deimelr@hotmail.com>
2017-02-27 20:42:28 +1100

    updating data

--- data.py
+++ data.py
@@ -7,19 +7,19 @@ users = {
             '192.168.1.150': {
                 'device': 'desktop',
                 'mac': 'e0:cb:4e:79:8a:5e',
-                'online': True,
+                'online': False,
                 'seen': [datetime.datetime.strptime('2017-02-26 01:22:09.489923', '%Y-%m-%d %H:%M:%S.%f'), '']
             },
-            '192.168.1.111': {
+            '192.168.1.149': {
                 'device': 'laptop',
                 'mac': '78:0c:b8:86:d5:e4',
-                'online': True,
+                'online': False,
                 'seen': [datetime.datetime.strptime('2017-02-26 01:22:09.489923', '%Y-%m-%d %H:%M:%S.%f'), '']
             },
             '192.168.1.105': {
                 'device': 'phone',
                 'mac': '50:2e:5c:ca:16:1c',
-                'online': True,
+                'online': False,
                 'seen': [datetime.datetime.strptime('2017-02-26 01:22:09.489923', '%Y-%m-%d %H:%M:%S.%f'), '']
             }
         }
@@ -29,18 +29,33 @@ users = {
             '192.168.1.106': {
                 'device': 'iPad',
                 'mac': '70:a2:b3:d9:dc:6b',
-                'online': True,
+                'online': False,
                 'seen': [datetime.datetime.strptime('2017-02-26 01:22:09.489923', '%Y-%m-%d %H:%M:%S.%f'), '']
             }
         }
     },
     'sahar': {
-        'ip': {}
+        'ip': {
+            '192.168.1.114': {
+                'device': 'Unknown',
+                'mac': '28:37:37:19:9c:de',
+                'online': False,
+                'seen': [datetime.datetime.strptime('2017-02-26 01:22:09.489923', '%Y-%m-%d %H:%M:%S.%f'), '']
+            }
+        }
     },
     'marc': {
-        'ip': {}
+        'ip': {
+            '192.168.1.103': {
+                'device': 'iPhone',
+                'mac': '70:70:0d:f3:1f:d3',
+                'online': False,
+                'seen': [datetime.datetime.strptime('2017-02-26 01:22:09.489923', '%Y-%m-%d %H:%M:%S.%f'), '']
+            }
+        }
     },
 }
+
 
 def update():
     with shelve.open('userdb') as db:
